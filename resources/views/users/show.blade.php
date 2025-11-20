a0f037f00c6cbb5a3202ed2917c40cefd2906976
ypervoreios <c.chorinos@gmail.com>
2025-11-20 13:05:20 +0200

    User Delete

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,16 +5,16 @@
         <p><strong>Username: </strong><?= $user->username ?></p>
     </div>
     <div class="bg-gray-200 p-5 rounded-lg shadow-lg mt-4">
-    <p><strong>Δικαιώματα:</strong>
-        @foreach ($user->roles as $role)
-            <?= $role->name ?>{{ !$loop->last ? ',' : '' }}
-        @endforeach
-    </p>
+        <p><strong>Δικαιώματα:</strong>
+            @foreach ($user->roles as $role)
+                <?= $role->name ?>{{ !$loop->last ? ',' : '' }}
+            @endforeach
+        </p>
     </div>
     <button onclick="window.history.back()" class="btn btn-secondary mt-3">Επιστροφή</button>
-    <form action="{{ route('users.delete', $user->id) }}" method="POST" class="d-inline">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-sm btn-outline-danger mt-3">Διαγραφή χρήστη</button>
-</form>
+    <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-inline">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-sm btn-outline-danger mt-3">Διαγραφή χρήστη</button>
+    </form>
 </x-layout>
